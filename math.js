77ecd4c5dfc8c800f295835f1e3bcfd2af7cf7fb
peterpaularetex-creator <peterpaul.aretex@gmail.com>
2026-02-16 13:35:53 +1100

    Fix: math.js file syntax

--- math.js
+++ math.js
@@ -1,6 +1,6 @@
 module.exports = {
-    add: (a, b) => a + b,
-    substract: (a, b) => a - b,
-    multiply: (a, b) => a * b,
-    divide: (a, b) => a / b,
+    add: (a, a) => a + b,
+    substract: (a, a) => a - b,
+    multiply: (a, a)  a * b,
+    divide
 };
